23c432a56c062e25797b55262deb9497acaa4581
Naveed Dogar <111642055+naveeddogar@users.noreply.github.com>
2023-08-25 17:06:42 +0500

    🧩τнοя м∂🔨

--- commands/system.js
+++ commands/system.js
@@ -3,7 +3,7 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md By Suhail
+ * @project_name : Thor-MD By Suhail
  * @author : SuhailTechInfo <https://github.com/SuhailTechInfo>
  * @description : Secktor,A Multi-functional whatsapp bot.
  * @version 0.0.6
